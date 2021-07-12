be4e8b258e9cdeb587e6d1345346cacb8378a882
aiusquare <ahmadibrahimusmanmkk@gmail.com>
2021-07-12 11:46:52 +0100

    initial

--- app.js
+++ app.js
@@ -2,30 +2,34 @@
 function payWithPaystack() {
 
     var handler = PaystackPop.setup({ 
-        key: 'your_public_key', //put your public key here
-        email: 'customer@email.com', //put your customer's email here
-        amount: 370000, //amount the customer is supposed to pay
-        metadata: {
-            custom_fields: [
-                {
-                    display_name: "Mobile Number",
-                    variable_name: "mobile_number",
-                    value: "+2348012345678" //customer's mobile number
-                }
-            ]
-        },
+        key: 'pk_test_d45a3431f0a021d77680cf52aad750bc2bd1efa7', //put your public key here
+        email: 'ahmadibrahimusmanmkk@gmail.com', //put your customer's email here
+        amount: 45680, //amount the customer is supposed to pay
+        currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+       	ref: ''+ Math.floor((Math.random()*1000000000)+1),
+       	metadata: {
+       		custom_fields: [
+       		{
+       			display_name: "Mobile Number",
+       			variable_name: "mobile_number",
+       			value: "+2349016609192"
+       		}
+       		]
+       	},
         callback: function (response) {
+        	window.location = "http://0.0.0.0:8080/Paystack-Payout-/verify.php?reference=" + response.reference;
             //after the transaction have been completed
             //make post call  to the server with to verify payment 
             //using transaction reference as post data
-            $.post("verify.php", {reference:response.reference}, function(status){
+            /*$.post("verify.php", {reference:response.reference}, function(status){
                 if(status == "success")
                     //successful transaction
                     alert('Transaction was successful');
                 else
                     //transaction failed
-                    alert(response);
-            });
+                    alert("there seems to be something"+response.reference+":"+status);
+            });*/
+            //alert("payment complete with:"+response.reference);
         },
         onClose: function () {
             //when the user close the payment modal
--- verify.php
+++ verify.php
@@ -1,36 +1,31 @@
 <?php
-//check if request was made with the right data
-if(!$_SERVER['REQUEST_METHOD'] == 'POST' || !isset($_POST['reference'])){  
-  die("Transaction reference not found");
-}
-
-//set reference to a variable @ref
-$reference = $_POST['reference'];
-
-//The parameter after verify/ is the transaction reference to be verified
-$url = 'https://api.paystack.co/transaction/verify/'.$reference;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt(
-  $ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer secret_key']
-);
-
-//send request
-$request = curl_exec($ch);
-//close connection
-curl_close($ch);
-//declare an array that will contain the result 
-$result = array();
-
-if ($request) {
-  $result = json_decode($request, true);
-}
-
-if (array_key_exists('data', $result) && array_key_exists('status', $result['data']) && ($result['data']['status'] === 'success')) {
-  echo "success";
-	//Perform necessary action
-}else{
-  echo "Transaction was unsuccessful";
-}
+	$refs = $_GET["reference"];
+  $curl = curl_init();
+  
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.paystack.co/transaction/verify/". rawurlencode($refs),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "Authorization: Bearer sk_test_b176a532708e53bd059c2b563ef6d49e44b5d677",
+      "Cache-Control: no-cache",
+    ),
+  ));
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  curl_close($curl);
+  
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    //echo $response;
+    //$return = json_decode($response);
+    //$status = $return -> data-> status;
+    echo $response.data.status;
+  }
+?>
